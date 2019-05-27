6c2d0c721c6420360c35f0f421879a379a45a7fa
weilun <weilun@xiaomi.com>
2019-05-28 06:00:37 +0800

    Initial Commit

---
+++ BaiDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiDeJie
+//
+//  Created by Lun Wei on 2019/5/28.
+//  Copyright © 2019 Lun Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
